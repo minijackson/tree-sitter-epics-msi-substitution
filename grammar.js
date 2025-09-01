38d50684f7dd5aa31d7fe7212e2fce4ed5c185ac
Minijackson <minijackson@riseup.net>
2025-09-01 09:23:10 +0200

    chore: import from epics-extensions/tree-sitter-epics

--- grammar.js
+++ grammar.js
@@ -1,5 +1,5 @@
 /**
- * @file EpicsMsiSubstitution grammar for tree-sitter
+ * @file tree-sitter grammar for EPICS' .substitutions files
  * @author Rémi NICOLE
  * @license MIT
  */
@@ -10,8 +10,54 @@
 module.exports = grammar({
   name: "epics_msi_substitution",
 
+  // TODO: Specifications says:
+  // A comment line is any line beginning with the character #, which must be the very first character on the line.
+  extras: ($) => [/\s/, $.comment, $.macro_expansion],
+
   rules: {
-    // NOTE: add the actual grammar rules
-    changeme: $ => "",
-  }
+    source_file: ($) =>
+      choice(repeat($.regular), repeat($.pattern), repeat($.db_template)),
+
+    comment: ($) => seq("#", /.*/),
+
+    regular: ($) =>
+      seq("{", repeat(seq($._regular_assignment, optional(","))), "}"),
+    _regular_assignment: ($) => seq($.identifier, "=", $.string),
+
+    pattern: ($) =>
+      seq("pattern", $.pattern_variables, repeat($.pattern_values)),
+    pattern_variables: ($) =>
+      seq("{", repeat(seq($.identifier, optional(","))), "}"),
+    pattern_values: ($) => seq("{", repeat(seq($.string, optional(","))), "}"),
+
+    db_template: ($) => seq("file", field("file", $.string), $.substitutions),
+
+    substitutions: ($) =>
+      seq("{", choice(repeat($.regular), repeat($.pattern)), "}"),
+
+    identifier: ($) => /\w+/,
+
+    string: ($) => choice($._bareword, $.quoted_string),
+
+	_bareword: ($) => /[a-zA-Z0-9_\-+:./\\\[\]<>;]+/,
+
+    quoted_string: ($) =>
+      seq(
+        '"',
+        repeat(choice($.escape_sequence, $.quoted_string_text_fragment)),
+        '"'
+      ),
+    quoted_string_text_fragment: ($) =>
+      prec.right(
+        repeat1(choice(token.immediate(/[^"\\$]+/), token.immediate("\\")))
+      ),
+    escape_sequence: ($) =>
+      choice(token.immediate('\\"'), token.immediate("\\\\")),
+
+    macro_expansion: ($) =>
+      choice(
+        seq("${", repeat1(choice(/[^}]+/, $.macro_expansion)), "}"),
+        seq("$(", repeat1(choice(/[^)]+/, $.macro_expansion)), ")")
+      ),
+  },
 });
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,1 +1,18 @@
-; NOTE: add highlight queries here
+(source_file) @local.scope
+
+(identifier) @identifier
+
+(string) @string
+(escape_sequence) @string.special
+(macro_expansion) @string.special
+
+(comment) @comment
+
+[ "${" "{" "}" "$(" ")" ] @punctuation.bracket
+"," @punctuation.delimiter
+"=" @punctuation.special
+
+[
+ "pattern"
+ "file"
+] @keyword
